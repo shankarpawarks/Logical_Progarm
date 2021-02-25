33f7a57f19755b6c8bcac16831fdbd7495ae1616
shankarpawarks@gmail.com <shankarpawarks@gmail.com>
2021-02-25 06:57:46 +0530

    logical count of vowels in given string

---
+++ Countvowels.java
@@ -0,0 +1,29 @@
+/*LOGICAL PRORAM FOR COUNT VOWEL IN THE GIVEN STRING */
+
+class Countvowels 
+{
+	public static void main(String[] args) 
+	{
+		String str="how are you?";
+		char ch[]=str.toCharArray();
+		int count=0;
+		
+		
+		
+		
+		for(char c:ch){
+			switch(c){
+				    case 'a':
+					case 'e':
+					case 'i':
+					case 'o':
+					case 'u':
+					count++;	
+					break;
+					
+				 
+			}
+		}
+		System.out.println("COUNT OF VOWEL IS::-"+count);
+	}
+}
