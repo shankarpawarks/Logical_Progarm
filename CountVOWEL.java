a3b4efb06a8578d6f430fe5530865573b1172c57
shankarpawarks@gmail.com <shankarpawarks@gmail.com>
2021-02-25 06:46:27 +0530

    VOWEL COUNT IN STRING

---
+++ CountVOWEL.java
@@ -0,0 +1,27 @@
+class CountVOWEL 
+{
+	public static void main(String[] args) 
+	{
+		String str="how are you?";
+		char ch[]=str.toCharArray();
+		int count=0;
+		
+		
+		
+		
+		for(char c:ch){
+			switch(c){
+				    case 'a':
+					case 'e':
+					case 'i':
+					case 'o':
+					case 'u':
+					count++;	
+					break;
+					
+				 
+			}
+		}
+		System.out.println("COUNT OF VOWEL IS::-"+count);
+	}
+}
